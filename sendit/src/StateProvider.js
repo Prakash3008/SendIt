d7b1f0fe8e4aa6987057cba7976a14be307a47d8
Kowreesh_3801 <kowreesh@gmail.com>
2020-09-16 00:32:17 +0530

    commit on 'first commit'

---
+++ sendit/src/StateProvider.js
@@ -0,0 +1,12 @@
+import React, {createContext, UseContext, useReducer} from "react";
+
+
+
+export const StateContext = createContext();
+
+export const StateProvider = ({reducer, initialState, children}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
