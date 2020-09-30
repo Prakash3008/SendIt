977b519200453f381d017ff31836bd54ff3e2abf
Prakash3008 <prak30ash@gmail.com>
2020-09-30 13:58:59 +0530

    prepare to deploy

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ io.on('connection', socket => {
 
 });
 
-if(process.env.PROD){
+if(process.env.NODE_ENV){
     app.use(express.static(path.join(__dirname, './sendit/build')));
     app.get('*', (req,res) =>{
-        res.sendFile(path.join(__dirname, './sendit/build/index.html'));
+        res.sendFile(path.join(__dirname, './sendit/build/index.html'))
     });
 }
 
